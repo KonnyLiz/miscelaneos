019682a345444fbb62ece9a4fd76e1c3864a7504
Carlos Padilla <konnylizamaya@gmail.com>
2021-11-30 14:28:57 -0600

    parametros entre padres e hijos

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: ActivatedRoute
+  ) {
+    router.params.subscribe(res => {
+      console.log('padre', res['id']);
+    });
+  }
 
   ngOnInit(): void {
   }
